c910cdb71dbda88e971a69976351bbb853bd24df
kolyasochnick <kosaval48@gmail.com>
2026-09-11 12:14:23 +0300

    Separate calculations

--- airfoil.py
+++ airfoil.py
@@ -1,8 +1,5 @@
-from math import cos, pi, atan, sin
-import matplotlib.pyplot as plt
-from matplotlib.widgets import Slider, Button
-
 from visualizer import AirfoilVisualizer
+from calculatuion import four_digit_airfoil_calculation
 
 class Airfoil:
     def __init__(self, camber=0.04, Xcamber=0.4, thickness=0.15, a=0.29690, b=0.126, c=0.3516, d=0.2843, e=0.10150):
@@ -17,7 +14,7 @@ class Airfoil:
         self.e = e
     
     def calculate(self):
-            return self.four_digit_airfoil_calculate(self.camber, 
+            return four_digit_airfoil_calculation(self.camber, 
                                self.Xcamber, 
                                self.thickness, 
                                self.a,
@@ -40,45 +37,6 @@ class Airfoil:
             for point in lower:
                 f.write(f'{point[0]} {point[1]}\n')
         print(f'Точки данного профиля сохранены по {path}')
-        
-        
-    def four_digit_airfoil_calculate(self, camber, Xcamber, thickness, a=0.29690, b=0.126, c=0.3516, d=0.2843, e=0.10150, N=100):
-        upper_surface_points = [] 
-        lower_surface_points = []
-        mean_line = []
-        for i in range(1, N + 1):
-            angle_of_sector = pi / (2 * (N - 1))
-            current_Xpoint = 1 - cos((i - 1) * angle_of_sector)
-    
-            thickness_value = 5 * thickness * (a * current_Xpoint ** 0.5 -
-                                                             b * current_Xpoint -
-                                                             c * current_Xpoint ** 2 + 
-                                                             d * current_Xpoint ** 3 - 
-                                                             e * current_Xpoint ** 4)
-            
-    
-            if current_Xpoint > Xcamber:
-                Y_of_mean_line =  camber * ((1 - 2 * Xcamber) + 2 * Xcamber * current_Xpoint - current_Xpoint ** 2)/ (1 - Xcamber) ** 2 
-                slope_mean_line = 2 * camber * (Xcamber - current_Xpoint) / (1 - Xcamber) ** 2
-    
-            else: 
-                Y_of_mean_line =  camber * ((2 * Xcamber - current_Xpoint) * current_Xpoint)/ Xcamber ** 2 
-                slope_mean_line = 2 * camber / Xcamber * (1 - (current_Xpoint / Xcamber))
-    
-            ordinate_rotation_angle = atan(slope_mean_line)
-    
-            upper_surface_X = current_Xpoint - thickness_value * sin(ordinate_rotation_angle)                
-            upper_surface_Y = Y_of_mean_line +  thickness_value * cos(ordinate_rotation_angle)
-        
-            lower_surface_X = current_Xpoint + thickness_value * sin(ordinate_rotation_angle)
-            lower_surface_Y = Y_of_mean_line - thickness_value * cos(ordinate_rotation_angle)
-    
-            upper_surface_points.append((round(upper_surface_X, 4), round(upper_surface_Y, 4)))
-            lower_surface_points.append((round(lower_surface_X, 4), round(lower_surface_Y, 4)))
-            
-            mean_line.append((current_Xpoint, Y_of_mean_line))
-                
-        return upper_surface_points, lower_surface_points, mean_line
     
         
 airfoil = Airfoil()
---
+++ calculatuion.py
@@ -0,0 +1,45 @@
+from math import cos, pi, atan, sin
+
+        
+    
+def four_digit_airfoil_calculation(camber, Xcamber, thickness, a, b, c, d, e, N):
+    upper_surface_points = [] 
+    lower_surface_points = []
+    mean_line = []
+    for i in range(1, N + 1):
+        angle_of_sector = pi / (2 * (N - 1))
+        current_Xpoint = 1 - cos((i - 1) * angle_of_sector)
+
+        thickness_value = 5 * thickness * (a * current_Xpoint ** 0.5 -
+                                                        b * current_Xpoint -
+                                                        c * current_Xpoint ** 2 + 
+                                                        d * current_Xpoint ** 3 - 
+                                                        e * current_Xpoint ** 4)
+        
+
+        if current_Xpoint > Xcamber:
+            Y_of_mean_line =  camber * ((1 - 2 * Xcamber) + 2 * Xcamber * current_Xpoint - current_Xpoint ** 2)/ (1 - Xcamber) ** 2 
+            slope_mean_line = 2 * camber * (Xcamber - current_Xpoint) / (1 - Xcamber) ** 2
+
+        else: 
+            Y_of_mean_line =  camber * ((2 * Xcamber - current_Xpoint) * current_Xpoint)/ Xcamber ** 2 
+            slope_mean_line = 2 * camber / Xcamber * (1 - (current_Xpoint / Xcamber))
+
+        ordinate_rotation_angle = atan(slope_mean_line)
+
+        upper_surface_X = current_Xpoint - thickness_value * sin(ordinate_rotation_angle)                
+        upper_surface_Y = Y_of_mean_line +  thickness_value * cos(ordinate_rotation_angle)
+    
+        lower_surface_X = current_Xpoint + thickness_value * sin(ordinate_rotation_angle)
+        lower_surface_Y = Y_of_mean_line - thickness_value * cos(ordinate_rotation_angle)
+
+        upper_surface_points.append((round(upper_surface_X, 4), round(upper_surface_Y, 4)))
+        lower_surface_points.append((round(lower_surface_X, 4), round(lower_surface_Y, 4)))
+        
+        mean_line.append((current_Xpoint, Y_of_mean_line))
+            
+    return upper_surface_points, lower_surface_points, mean_line
+    
+
+if __name__ == '__main__':
+    print('Running calculation.py')
